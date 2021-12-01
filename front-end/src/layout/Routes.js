7c4b501dce3c89800badd01d930d4fdec4124c23
Angela Louh <louh.angela@gmail.com>
2021-11-30 17:23:08 -0800

    Changed component name from ReservationForm to ReservationCreate

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -4,7 +4,7 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
-import ReservationForm from "../reservations/new/ReservationForm";
+import ReservationCreate from "../reservations/new/ReservationCreate";
 
 /**
  * Defines all the routes for the application.
@@ -20,7 +20,7 @@ function Routes() {
         <Redirect to={"/dashboard"} />
       </Route>
       <Route path="/reservations/new">
-        <ReservationForm />
+        <ReservationCreate />
       </Route>
       <Route exact={true} path="/reservations">
         <Redirect to={"/dashboard"} />
