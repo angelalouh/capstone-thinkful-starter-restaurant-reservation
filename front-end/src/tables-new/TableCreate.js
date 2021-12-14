114f8ad7f17a7e1d5b04c1fe3fe31a6a4b94065b
Angela Louh <louh.angela@gmail.com>
2021-12-14 02:00:34 -0800

    Added styling and new TableCreateFormButtons component

--- front-end/src/tables-new/TableCreate.js
+++ front-end/src/tables-new/TableCreate.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { createTable } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
+import TableCreateFormButtons from "./TableCreateFormButtons";
 
 function TableCreate() {
   const history = useHistory();
@@ -18,10 +19,6 @@ function TableCreate() {
     }));
   }
 
-  function cancelHandler() {
-    history.goBack();
-  }
-
   function submitHandler(event) {
     event.preventDefault();
     const abortController = new AbortController();
@@ -34,10 +31,10 @@ function TableCreate() {
 
   return (
     <main>
-      <h1>Create Table</h1>
+      <h1 class="mt-lg-4 mt-1 mb-3">Create Table</h1>
       <ErrorAlert error={error} />
       <form onSubmit={submitHandler}>
-        <div class="col-5 mb-3 pl-1">
+        <div class="col-md-6 col-xl-4 mb-3 pl-1">
           <label htmlFor="table_name" class="form-label">
             Table Name:
           </label>
@@ -54,7 +51,7 @@ function TableCreate() {
             Table name must be at least 2 characters long.
           </div>
         </div>
-        <div class="col-5 mb-3 pl-1">
+        <div class="col-md-6 col-xl-4 mb-3 pl-1">
           <label htmlFor="capacity" class="form-label">
             Capacity:
           </label>
@@ -72,39 +69,8 @@ function TableCreate() {
             Table capacity must be at least 1.
           </div>
         </div>
-        <div>
-          <button
-            type="button"
-            class="btn btn-danger mr-2 ml-1"
-            onClick={cancelHandler}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-x-circle mr-2 mb-1"
-              viewBox="0 0 16 16"
-            >
-              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-            </svg>
-            Cancel
-          </button>
-          <button type="submit" class="btn btn-success">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-check-circle mr-2 mb-1"
-              viewBox="0 0 16 16"
-            >
-              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-              <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
-            </svg>
-            Submit
-          </button>
+        <div class="col-md-6 col-xl-4 col-auto">
+          <TableCreateFormButtons history={history} />
         </div>
       </form>
     </main>
