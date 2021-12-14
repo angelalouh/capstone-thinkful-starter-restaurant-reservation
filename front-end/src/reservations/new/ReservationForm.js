190adff8e5a16a57a543904eeb986a55569c4b20
Angela Louh <louh.angela@gmail.com>
2021-12-14 02:01:14 -0800

    Added styling to reservation form

--- front-end/src/reservations/new/ReservationForm.js
+++ front-end/src/reservations/new/ReservationForm.js
@@ -17,7 +17,7 @@ function ReservationForm({ reservation, setReservation }) {
 
   return (
     <>
-      <div class="col-md-5">
+      <div class="col-md-6 col-lg-6">
         <label htmlFor="first_name" class="form-label">
           First Name:
         </label>
@@ -31,7 +31,7 @@ function ReservationForm({ reservation, setReservation }) {
           value={reservation.first_name}
         />
       </div>
-      <div class="col-md-5">
+      <div class="col-md-6 col-lg-6">
         <label htmlFor="last_name" class="form-label">
           Last Name:
         </label>
@@ -45,7 +45,7 @@ function ReservationForm({ reservation, setReservation }) {
           value={reservation.last_name}
         />
       </div>
-      <div class="col-md-3">
+      <div class="col-md-6 col-lg-4">
         <label htmlFor="mobile_number" class="form-label">
           Mobile Number:
         </label>
@@ -62,7 +62,7 @@ function ReservationForm({ reservation, setReservation }) {
           Format: XXX-XXX-XXXX or XXX-XXXX
         </div>
       </div>
-      <div class="col-md-4">
+      <div class="col-md-6 col-lg-4">
         <label htmlFor="people" class="form-label">
           Number of People in Party:
         </label>
@@ -80,7 +80,7 @@ function ReservationForm({ reservation, setReservation }) {
           Party size must range from 1-6
         </div>
       </div>
-      <div class="col-md-3">
+      <div class="col-md-4 col-lg-4">
         <label htmlFor="reservation_date" class="form-label">
           Date of Reservation:
         </label>
@@ -88,73 +88,61 @@ function ReservationForm({ reservation, setReservation }) {
           id="reservation_date"
           name="reservation_date"
           type="date"
-          class="form-control"
+          class="form-control text-center"
           pattern="\d{4}-\d{2}-\d{2}"
           require="true"
           onChange={changeHandler}
           value={reservation.reservation_date}
         />
       </div>
-      <div class="row g-2 align-items-center pl-3">
-        <div class="col-md-3 pr-0">
-          <label htmlFor="reservation_time" class="form-label">
-            Time of Reservation:
-          </label>
-          {/* </div> */}
-          {/* <div class="col-auto pr-0"> */}
-          <input
-            id="reservation_time"
-            name="reservation_time"
-            type="time"
-            class="form-control"
-            pattern="[0-9]{2}:[0-9]{2}"
-            require="true"
-            onChange={changeHandler}
-            value={reservation.reservation_time}
-          />
+      <div class="col-md-4 col-lg-3 pr-3">
+        <label htmlFor="reservation_time" class="form-label">
+          Time of Reservation:
+        </label>
+        <input
+          id="reservation_time"
+          name="reservation_time"
+          type="time"
+          class="form-control text-center"
+          pattern="[0-9]{2}:[0-9]{2}"
+          require="true"
+          onChange={changeHandler}
+          value={reservation.reservation_time}
+        />
+        <div id="reservationTimeHelp" class="form-text">
+          Business Hours: 10:30AM to 10:30PM (latest reservation allowed:
+          9:30PM)
         </div>
-        <div class="col-md-4">
-          <span id="reservationTimeHelp" class="form-text col-auto pl-0 mt-4">
-            Business Hours: 10:30AM to 10:30PM (latest reservation allowed:
-            9:30PM)
-          </span>
-        </div>
-        <div class="col-md-5 pl-1 mt-4">
-          {/* <div class="d-grid gap-2 d-md-flex justify-content-md-center"> */}
-          <button
-            type="button"
-            class="btn btn-danger mr-2"
-            onClick={cancelHandler}
+      </div>
+      <div class="mt-4 d-grid gap-3 d-flex justify-content-end pr-3">
+        <button type="button" class="btn btn-danger" onClick={cancelHandler}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-calendar-x mr-2 mb-1"
+            viewBox="0 0 16 16"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-calendar-x mr-2 mb-1"
-              viewBox="0 0 16 16"
-            >
-              <path d="M6.146 7.146a.5.5 0 0 1 .708 0L8 8.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 9l1.147 1.146a.5.5 0 0 1-.708.708L8 9.707l-1.146 1.147a.5.5 0 0 1-.708-.708L7.293 9 6.146 7.854a.5.5 0 0 1 0-.708z" />
-              <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
-            </svg>
-            Cancel
-          </button>
-          <button type="submit" class="btn btn-success">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-calendar-check mr-2 mb-1"
-              viewBox="0 0 16 16"
-            >
-              <path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
-              <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
-            </svg>
-            Submit
-          </button>
-          {/* </div> */}
-        </div>
+            <path d="M6.146 7.146a.5.5 0 0 1 .708 0L8 8.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 9l1.147 1.146a.5.5 0 0 1-.708.708L8 9.707l-1.146 1.147a.5.5 0 0 1-.708-.708L7.293 9 6.146 7.854a.5.5 0 0 1 0-.708z" />
+            <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+          </svg>
+          Cancel
+        </button>
+        <button type="submit" class="btn btn-success">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-calendar-check mr-2 mb-1"
+            viewBox="0 0 16 16"
+          >
+            <path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
+            <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+          </svg>
+          Submit
+        </button>
       </div>
     </>
   );
